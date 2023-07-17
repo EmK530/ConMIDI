01a4bdca48e9aebfe36a7b6c250f8d29b26ef708
Emil <50961259+EmK530@users.noreply.github.com>
2023-07-17 23:07:37 +0200

    More GetProcAddress prints

--- Sound/KDMAPI.c
+++ Sound/KDMAPI.c
@@ -9,30 +9,18 @@ KDM_INIT KDMAPI_TerminateKDMAPIStream;
 KDM_INIT KDMAPI_ResetKDMAPIStream;
 KDM_SEND KDMAPI_SendDirectData;
 int KDMAPI_Setup(){
-    if ((KDMAPI_libHandle = LoadLibrary("OmniMIDI")) == NULL)
-    {
-        printf("KDMAPI not available.\n");
-        return 0;
-    }
-    if ((KDMAPI_InitializeKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "InitializeKDMAPIStream")) == NULL)
-    {
-        printf("GetProcAddress failed for KDMAPI.\n");
-        printf("%d\n", GetLastError());
-        return 0;
-    }
+    if ((KDMAPI_libHandle = LoadLibrary("OmniMIDI")) == NULL){printf("KDMAPI not available.\n");return 0;}
+    if ((KDMAPI_InitializeKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "InitializeKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI InitializeKDMAPIStream.\n");return 0;}
     static KDM_SEND cached_KDMAPI_SendDirectData = NULL;
-    if (!cached_KDMAPI_SendDirectData)
-    {
+    if (!cached_KDMAPI_SendDirectData){
         cached_KDMAPI_SendDirectData = (KDM_SEND)GetProcAddress(KDMAPI_libHandle, "SendDirectData");
-        if (!cached_KDMAPI_SendDirectData)
-        {
-            printf("GetProcAddress failed for SendDirectData.\n");
-            printf("%d\n", GetLastError());
+        if (!cached_KDMAPI_SendDirectData){
+            printf("GetProcAddress failed for KDMAPI SendDirectData.\n");
             return 0;
         }
     }
-    KDMAPI_TerminateKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "TerminateKDMAPIStream");
-    KDMAPI_ResetKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "ResetKDMAPIStream");
+    if ((KDMAPI_TerminateKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "TerminateKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI TerminateKDMAPIStream.\n");return 0;}
+    if ((KDMAPI_ResetKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "ResetKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI ResetKDMAPIStream.\n");return 0;}
     printf("KDMAPI FUNCTIONAL\n");
     KDMAPI_SendDirectData = cached_KDMAPI_SendDirectData;
     return 1;
