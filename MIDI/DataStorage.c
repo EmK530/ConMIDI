1a2041c67970b9d0a447c1270f68d4c81ce6de3f
Emil <50961259+EmK530@users.noreply.github.com>
2023-07-18 18:04:56 +0200

    Modify structs

--- MIDI/DataStorage.c
+++ MIDI/DataStorage.c
@@ -4,10 +4,15 @@ unsigned int format = 0;
 unsigned int realTracks = 0;
 unsigned long long notes = 0;
 struct SynthEvent {
+    unsigned long int pos;
+    unsigned long int event;
+};
+struct Tempo {
     float pos;
+    unsigned long int offset;
     unsigned long int event;
 };
 struct SynthEvent **SynthEvents;
-struct SynthEvent **Tempos;
+struct Tempo **Tempos;
 unsigned long int *eventCounts;
 unsigned long int *tempoCounts;
