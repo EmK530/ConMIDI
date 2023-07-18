a791187106f2adc603f9e24c2df39a217d175c50
Emil <50961259+EmK530@users.noreply.github.com>
2023-07-18 16:25:54 +0200

    Revert recent changes

--- Sound/KDMAPI.c
+++ Sound/KDMAPI.c
@@ -11,9 +11,17 @@ KDM_SEND KDMAPI_SendDirectData;
 int KDMAPI_Setup(){
     if ((KDMAPI_libHandle = LoadLibrary("OmniMIDI")) == NULL){printf("KDMAPI not available.\n");return 0;}
     if ((KDMAPI_InitializeKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "InitializeKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI InitializeKDMAPIStream.\n");return 0;}
-    if ((KDMAPI_SendDirectData = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "SendDirectData")) == NULL){printf("GetProcAddress failed for KDMAPI SendDirectData.\n");return 0;}
+    static KDM_SEND cached_KDMAPI_SendDirectData = NULL;
+    if (!cached_KDMAPI_SendDirectData){
+        cached_KDMAPI_SendDirectData = (KDM_SEND)GetProcAddress(KDMAPI_libHandle, "SendDirectData");
+        if (!cached_KDMAPI_SendDirectData){
+            printf("GetProcAddress failed for KDMAPI SendDirectData.\n");
+            return 0;
+        }
+    }
     if ((KDMAPI_TerminateKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "TerminateKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI TerminateKDMAPIStream.\n");return 0;}
     if ((KDMAPI_ResetKDMAPIStream = (KDM_INIT)GetProcAddress(KDMAPI_libHandle, "ResetKDMAPIStream")) == NULL){printf("GetProcAddress failed for KDMAPI ResetKDMAPIStream.\n");return 0;}
     printf("KDMAPI FUNCTIONAL\n");
+    KDMAPI_SendDirectData = cached_KDMAPI_SendDirectData;
     return 1;
 }
