586bc0774fd3ca7aafb433204b98dd2f34b9f547
Emil <50961259+EmK530@users.noreply.github.com>
2023-07-18 19:38:56 +0200

    Add ntdll functions

--- Essentials.c
+++ Essentials.c
@@ -2,12 +2,8 @@ _Pragma("GCC diagnostic ignored \"-Wimplicit-function-declaration\"");
 
 #include <stdio.h>
 #include <time.h>
+#include <windows.h>
 
-long long timeInMilliseconds(void) {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
-}
 int intInput(char* text){
     int num;
     printf(text);
@@ -38,4 +34,30 @@ char* concat(const char* str1, const char* str2) {
     strcpy(result, str1);
     strcat(result, str2);
     return result;
+}
+HMODULE ntdll_libHandle;
+char ntdllAvailable = 0;
+typedef unsigned int(__stdcall *NtDE)(unsigned char, signed long long);
+typedef unsigned int(__stdcall *NtQST)(signed long long*);
+NtDE NtDelayExecution;
+NtQST NtQuerySystemTime;
+double getTimeMsec(void) {
+    if(ntdllAvailable==1){
+        signed long long ret;
+        NtQuerySystemTime(&ret);
+        return (double)ret/10000;
+    } else {
+        struct timeval tv;
+        gettimeofday(&tv,NULL);
+        return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+    }
+}
+void NtSleep(signed long long t){
+    NtDelayExecution(0, t);
+}
+void setupntdll(){
+    if ((ntdll_libHandle = LoadLibrary("ntdll")) == NULL){printf("Could not load ntdll\n\n");return;}
+    ntdllAvailable=1;
+    if ((NtDelayExecution = (NtDE)GetProcAddress(ntdll_libHandle, "NtDelayExecution")) == NULL){printf("GetProcAddress failed for ntdll NtDelayExecution.\n\n");}
+    if ((NtQuerySystemTime = (NtQST)GetProcAddress(ntdll_libHandle, "NtQuerySystemTime")) == NULL){printf("GetProcAddress failed for ntdll NtQuerySystemTime.\n\n");}
 }
