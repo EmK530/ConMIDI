2783db715b05a9b00ee3a819494ca5f1456ba6a4
Emil <50961259+EmK530@users.noreply.github.com>
2023-08-04 02:42:01 +0200

    Rename version

--- ConMIDI.c
+++ ConMIDI.c
@@ -6,7 +6,7 @@
 #include "MIDI/LoadMIDI.c"
 
 FILE *file_ptr;
-char version[] = "v1.1.1-rawstore";
+char version[] = "v1.1.2-rawstore";
 char* title;
 int main(){
     printf("ConMIDI %s\n\n",version);
