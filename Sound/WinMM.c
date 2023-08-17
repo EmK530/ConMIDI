28e3cab65fb6a478cb29226b1dc80ba0615dbf59
Emil <50961259+EmK530@users.noreply.github.com>
2023-08-17 10:06:12 +0200

    Change WM_4 return type to void

--- Sound/WinMM.c
+++ Sound/WinMM.c
@@ -5,7 +5,7 @@ HMODULE WinMM_libHandle;
 typedef int(__stdcall *WM_1)();
 typedef int(__stdcall *WM_2)(UINT_PTR, LPMIDIOUTCAPSW, UINT);
 typedef int(__stdcall *WM_3)(LPHMIDIOUT,UINT,DWORD_PTR,DWORD_PTR,DWORD);
-typedef int(__stdcall *WM_4)(HMIDIOUT,DWORD);
+typedef void(__stdcall *WM_4)(HMIDIOUT,unsigned long int);
 WM_1 WinMM_midiOutGetNumDevs;
 WM_2 WinMM_midiOutGetDevCaps;
 WM_3 WinMM_midiOutOpen;
