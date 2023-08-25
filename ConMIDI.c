99992979d7599c873d1bb7b2805ba39cca70a946
Emil <50961259+EmK530@users.noreply.github.com>
2023-08-25 15:51:10 +0200

    Fix revert, whoops x2

--- ConMIDI.c
+++ ConMIDI.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]){
                 {
                     unsigned int i;
                     sscanf(argv[i], "%u", &i);
-                    SetConsoleOutputCP(updateCP);
+                    SetConsoleOutputCP(i);
                 }
             }
         }
