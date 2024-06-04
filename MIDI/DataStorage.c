c6be1625e10886d339eb57af32e722f95eaf9264
Lurmog <zezz06ar@gmail.com>
2024-06-04 22:19:48 +0200

    fix header file and add header guards

--- MIDI/DataStorage.c
+++ MIDI/DataStorage.c
@@ -1,11 +1,6 @@
-#ifndef DATASTORAGE_H
-#define DATASTORAGE_H
-
 unsigned int ppq = 0;
 unsigned int fakeTracks = 0;
 unsigned int format = 0;
 unsigned int realTracks = 0;
 unsigned long long notes = 0;
 unsigned char **tracks;
-
-#endif
