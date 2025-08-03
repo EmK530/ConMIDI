fedc4a88363e4be93904ea7b094a5584e9792a61
Emil <50961259+EmK530@users.noreply.github.com>
2025-08-03 13:38:12 +0200

    Delete MIDI directory

--- MIDI/DataStorage.c
+++
@@ -1,6 +0,0 @@
-unsigned int ppq = 0;
-unsigned int fakeTracks = 0;
-unsigned int format = 0;
-unsigned int realTracks = 0;
-unsigned long long notes = 0;
-unsigned char **tracks;
--- MIDI/LoadMIDI.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOADMIDI_H
-#define LOADMIDI_H
-
-void LoadMIDI(char path[], unsigned int bs);
-
-#endif
